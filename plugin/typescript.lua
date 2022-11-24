19d2c4282ca0214ca032944403fc181b16ac97ce
Xavier Hoffmann <hoffmann.xav@gmail.com>
2022-11-24 18:15:24 +0100

    window setups

--- plugin/typescript.lua
+++ plugin/typescript.lua
@@ -16,7 +16,5 @@ end
 
 -- TypeScript
 nvim_lsp.tsserver.setup {
-  on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  cmd = { "typescript-language-server", "--stdio" }
+  on_attach = on_attach
 }
